ca20201937b72383a681b2a250860015286d2ed3
Ketan Gondaliya <gondaliya900@gmail.com>
2019-07-01 22:07:06 +0530

    contact us section added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 
 // navigation bar
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('nav').addClass('shrink');
-    } else {
-      $('nav').removeClass('shrink');
-    }
-  });
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
+
+
+// contact us
+$(".cusinp").focus(function () {
+  $(this).parent().addClass("focus");
+}).blur(function () {
+  if ($(this).val() === '') {
+    $(this).parent().removeClass("focus");
+  }
+})
+
+//to solve contact us reload focus problem
+$(document).ready(function () {
+  if ($(".cusinp").val() != "") {
+    $(".cusinp").parent().addClass("focus");
+  }
+});
